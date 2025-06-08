a671490c20367ea68ff040a7bf77bd82a88cf4c3
Gavin D'souza <gavin18d@gmail.com>
2025-06-08 14:11:00 +0200

    feat: bench subcommand

---
+++ cmd/bench.go
@@ -0,0 +1,28 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"syscall"
+
+	"github.com/spf13/cobra"
+)
+
+var benchCmd = &cobra.Command{
+	Use:   "bench [args]",
+	Short: "Run bench commands via weg",
+	Run: func(cmd *cobra.Command, args []string) {
+		benchCli, err := exec.LookPath("bench")
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			return
+		}
+		execArgs := append([]string{"bench"}, args...)
+		syscall.Exec(benchCli, execArgs, os.Environ())
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(benchCmd)
+}
